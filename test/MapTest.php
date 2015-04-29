7d2658d4d5196b9c6d0e3c2a3b7269b42a4bc0a0
Levi Morrison <levim@php.net>
2015-04-28 21:01:07 -0600

    Add basic unit tests for map

---
+++ test/MapTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace morrisonlevi\Algorithm;
+
+
+class MapTest extends \PHPUnit_Framework_TestCase {
+
+
+    function test_empty() {
+        $function = function() {
+            $this->assertTrue(false);
+        };
+
+        $input = [];
+        $expect = $input;
+        $actual = iterator_to_array(map($function, $input));
+        $this->assertEquals($expect, $actual);
+    }
+
+
+    function test_basic() {
+        $function = function($value) {
+            return $value * 2;
+        };
+
+        $input = [1, 2, 3];
+        $expect = [2, 4, 6];
+        $actual = iterator_to_array(map($function, $input));
+        $this->assertEquals($expect, $actual);
+    }
+
+
+}
