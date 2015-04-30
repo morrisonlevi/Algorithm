de468cd195a74832cabbbc0993dc1512daf46119
Levi Morrison <levim@php.net>
2015-04-30 10:10:53 -0600

    Micro perf optimization in chain

--- src/chain.php
+++ src/chain.php
@@ -6,8 +6,9 @@ namespace morrisonlevi\Algorithm;
 function chain(callable $fn, callable ...$callables) {
     $functions = func_get_args();
     return function(...$params) use($functions) {
+        $count = count($functions);
         $carry = $functions[0](...$params);
-        for ($i = 1; $i < count($functions); $i++) {
+        for ($i = 1; $i < $count; $i++) {
             $carry = $functions[$i]($carry);
         }
         return $carry;
