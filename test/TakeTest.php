03fc4a3f11cdb47c7133ab6f3043e87cdfc9e2af
Levi Morrison <levim@php.net>
2015-09-10 22:31:06 -0600

    Rename methods in take's tests

--- test/TakeTest.php
+++ test/TakeTest.php
@@ -6,40 +6,40 @@ namespace morrisonlevi\Algorithm;
 class TakeTest extends \PHPUnit_Framework_TestCase {
 
 
-    function test_takeWhile_none() {
-        $takeWhile = take(function($value) {
+    function test_take_none() {
+        $take = take(function($value) {
             return $value % 2 > 0;
         });
 
         $input = [0,1,2];
         $expect = [];
-        $actual = iterator_to_array($takeWhile($input));
+        $actual = iterator_to_array($take($input));
 
         $this->assertEquals($expect, $actual);
     }
 
 
-    function test_takeWhile_once() {
-        $takeWhile = take(function($value) {
+    function test_take_once() {
+        $take = take(function($value) {
             return $value % 2 > 0;
         });
 
         $input = [1,2,3];
         $expect = [1];
-        $actual = iterator_to_array($takeWhile($input));
+        $actual = iterator_to_array($take($input));
 
         $this->assertEquals($expect, $actual);
     }
 
 
-    function test_takeWhile_all() {
-        $takeWhile = take(function($value) {
+    function test_take_all() {
+        $take = take(function($value) {
             return $value % 2 > 0;
         });
 
         $input = [1,3,5];
         $expect = $input;
-        $actual = iterator_to_array($takeWhile($input));
+        $actual = iterator_to_array($take($input));
 
         $this->assertEquals($expect, $actual);
     }
