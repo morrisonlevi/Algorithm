7fa6008efa0ef8887d732f62a9fc6293323e7c8a
Levi Morrison <levim@php.net>
2015-09-14 08:57:10 -0600

    Split sources list into its own file

---
+++ sources.mk
@@ -0,0 +1,19 @@
+SOURCES= \
+	src/bind.php \
+	src/chain.php \
+	src/chunk.php \
+	src/compose.php \
+	src/concat.php \
+	src/filter.php \
+	src/indexBy.php \
+	src/map.php \
+	src/mapcat.php \
+	src/max.php \
+	src/min.php \
+	src/product.php \
+	src/reduce.php \
+	src/skip.php \
+	src/skipN.php \
+	src/sum.php \
+	src/take.php \
+	src/takeN.php \
