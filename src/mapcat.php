4b512cf61eaf56c13c02dd603a3c1db88b6d62cd
Levi Morrison <levim@php.net>
2017-08-03 10:46:11 -0600

    Update mapcat comment

--- src/mapcat.php
+++ src/mapcat.php
@@ -4,11 +4,10 @@ namespace morrisonlevi\Algorithm;
 
 
 function mapcat(callable $fn) {
-    // This could be implemented in one line:
-    // return concat(map($fn));
-    // However, I opted to not do this since both map and cat have properties
-    // that aren't needed for this specific application and I prefer to use
-    // as few inter-dependencies as possible
+    // This could be implemented using map and concat/flatten
+    // However, I opted to not do this since map, cat and flatten have
+    // properties that aren't needed for this specific application and I
+    // prefer to use as few inter-dependencies as possible
     return function ($input) use($fn) {
         foreach ($input as $values) {
             foreach ($fn($values) as $key => $value) {
